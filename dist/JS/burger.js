ddfee283332884d4bc9b2576f866d12cfff740f3
Vladislav Vagner <vladislawwagner@gmail.com>
2023-10-03 17:25:59 +0200

    created header

---
+++ dist/JS/burger.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('.burger');
+const lists = document.querySelectorAll('.header-menu-list');
+const menus = document.querySelectorAll('.header-menu');
+
+burger.addEventListener('click', () => {
+	for (const list of lists) {
+		list.classList.toggle('header-menu-list_collapsed');
+	}
+	for (const menu of menus) {
+		menu.classList.toggle('header-menu_collapsed');
+	}
+});
