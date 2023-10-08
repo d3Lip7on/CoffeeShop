e4ff079cfd6ad4f4e11c7ee00e1d5447add401e6
Vladislav Vagner <vladislawwagner@gmail.com>
2023-10-08 15:01:59 +0200

    header animation

--- dist/JS/burger.js
+++ dist/JS/burger.js
@@ -1,14 +1,32 @@
+'use strict';
+
 const burger = document.querySelector('.burger');
 const lists = document.querySelectorAll('.header-menu-list');
 const menus = document.querySelectorAll('.header-menu');
 
 burger.addEventListener('click', () => {
-	for (const list of lists) {
-		list.classList.toggle('header-menu-list_collapsed');
-	}
-
-	for (const menu of menus) {
-		menu.classList.toggle('header-menu_collapsed');
+	if (burger.classList.contains('burger_rotated')) {
+		for (const list of lists) {
+			list.classList.add('header-menu-list_closing');
+			list.onanimationend = () => {
+				list.classList.remove('header-menu-list_closing');
+				list.classList.remove('header-menu-list_collapsed');
+				for (const menu of menus) {
+					menu.classList.remove('header-menu_collapsed');
+				}
+			};
+		}
+	} else {
+		for (const menu of menus) {
+			menu.classList.add('header-menu_collapsed');
+		}
+		for (const list of lists) {
+			list.classList.add('header-menu-list_collapsed');
+			list.classList.add('header-menu-list_opening');
+			list.onanimationend = () => {
+				list.classList.remove('header-menu-list_opening');
+			};
+		}
 	}
 
 	burger.classList.toggle('burger_rotated');
