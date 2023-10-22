0850cd0bc062594b9b70ae22a64b9ebab72c5738
Vladislav Vagner <vladislawwagner@gmail.com>
2023-10-22 15:03:43 +0200

    some fixes

--- JS/slider.js
+++ JS/slider.js
@@ -32,14 +32,13 @@ for (let slider of sliders) {
 	let mouseX;
 
 	sliderWindow.addEventListener('touchstart', (event) => {
-		event.preventDefault();
 		slides.style.transition = 'none';
 		isMousePressed = true;
 		initialPoint = event.touches[0].clientX - rectSliderWindow.left;
 	});
 
 	slider.addEventListener('touchend', (event) => {
-		if (mouseX - initialPoint < -sliderWindow.clientWidth / 2) {
+		if (mouseX - initialPoint < -sliderWindow.clientWidth / 3) {
 			if (currentSlidesPoint != -sliderWindow.clientWidth * (slide.length - 1)) {
 				currentSlidesPoint = currentSlidesPoint - sliderWindow.clientWidth;
 				indicators[indexOfActiveIndicator].classList.remove('slider__indicator_active');
@@ -47,7 +46,7 @@ for (let slider of sliders) {
 				indicators[indexOfActiveIndicator].classList.add('slider__indicator_active');
 			}
 		}
-		if (mouseX - initialPoint > sliderWindow.clientWidth / 2) {
+		if (mouseX - initialPoint > sliderWindow.clientWidth / 3) {
 			if (currentSlidesPoint != 0) {
 				currentSlidesPoint = currentSlidesPoint + sliderWindow.clientWidth;
 				indicators[indexOfActiveIndicator].classList.remove('slider__indicator_active');
@@ -62,9 +61,8 @@ for (let slider of sliders) {
 	});
 
 	slider.addEventListener('touchmove', (event) => {
-		event.preventDefault();
 		if (!isMousePressed) return;
 		mouseX = event.touches[0].clientX - rectSliderWindow.left;
-		slides.style.left = currentSlidesPoint + mouseX - initialPoint + 'px';
+		if (Math.abs(mouseX - initialPoint) > 10) slides.style.left = currentSlidesPoint + mouseX - initialPoint + 'px';
 	});
 }
